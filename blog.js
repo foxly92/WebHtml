c2dc991409de1b750aed076fb256cf0300e55dd4
foxly92 <166406919+foxly92@users.noreply.github.com>
2024-05-07 17:32:33 +0900

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,49 @@
+ 
+
+
+ 
+ 
+ let numbers = [3, 5, 7, 2, 8, 1, 4, 13, 10];
+ let temp;
+
+
+ numbers.forEach(function (number) {
+
+
+
+   
+   for(let i = 0; i <= numbers.length; i++) {
+
+    if (number > 5) {
+      
+      temp = number;
+      console.log(temp+" ");
+      
+      }
+   }
+
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ //let numbers = [3, 5, 7, 2, 8, 1, 4] 에서 5이상 숫자만 콘솔에 출력하는 프로그램 작성 (foreach 사용)
+
+ //다음 배열의 각 숫자에 10을 더한 후 , 새 배열 newNumber에 저장하는 프로그램 작성
+
+ //let numbers = [1,  2, 3, 4, 5];
+
+ //let newNumbers = [];
